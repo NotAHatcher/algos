f9b108a2699aeb6cdbb1d3595e7ad6e1304ef23c
NotAHatcher <dfgsgk1234@gmail.com>
2026-04-04 14:06:47 +1000

    22

--- #2073.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
-        s = len(tickets) * tickets[k] - (len(tickets)-(k+1))
-        for i in range(len(tickets)):
-            if tickets[i] < tickets[k]:
-                if i>k:
-                    s += 1
-                s-=(tickets[k] - tickets[i])
-        return s
--- classwork1.py
+++
@@ -1,6 +0,0 @@
-def all_unique(array):
-    return len(array) == len(set(array))
-
-print(all_unique([1,2,3,4]))
-print(all_unique([1,2,2,4]))
-print(all_unique([]))
